6741508bf351911dd4de9bcbb177d4358e498e63
gurupratap-matharu <gurupratap.matharu@gmail.com>
2019-11-29 20:33:19 -0300

    widget to render home page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:planets/ui/home/gradient_app_bar.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Hello'),
-    );
+        body: Column(
+      children: <Widget>[
+        GradientAppBar(
+          title: 'treva',
+        ),
+      ],
+    ));
   }
 }
