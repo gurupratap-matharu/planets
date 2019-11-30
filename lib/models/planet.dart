d5138519b781f6e3f456ec7b9ba2111bace7dc29
gurupratap-matharu <gurupratap.matharu@gmail.com>
2019-11-30 10:41:13 -0300

    added images of different planets

---
+++ lib/models/planet.dart
@@ -0,0 +1,19 @@
+class Planet {
+  Planet({
+    this.id,
+    this.name,
+    this.location,
+    this.distance,
+    this.description,
+    this.gravity,
+    this.image,
+  });
+
+  final String id;
+  final String name;
+  final String location;
+  final String distance;
+  final String gravity;
+  final String description;
+  final String image;
+}
---
+++ lib/models/planet_data.dart
@@ -0,0 +1,49 @@
+import 'package:planets/data/planet_class.dart';
+
+List<Planet> planets = [
+  Planet(
+    id: "1",
+    name: "Mars",
+    location: "Milkyway Galaxy",
+    distance: "227.9m Km",
+    gravity: "3.711 m/s ",
+    description: "Lorem ipsum...",
+    image: "assets/img/mars.png",
+  ),
+  Planet(
+    id: "2",
+    name: "Neptune",
+    location: "Milkyway Galaxy",
+    distance: "54.6m Km",
+    gravity: "11.15 m/s ",
+    description: "Lorem ipsum...",
+    image: "assets/img/neptune.png",
+  ),
+  Planet(
+    id: "3",
+    name: "Moon",
+    location: "Milkyway Galaxy",
+    distance: "54.6m Km",
+    gravity: "1.622 m/s ",
+    description: "Lorem ipsum...",
+    image: "assets/img/moon.png",
+  ),
+  Planet(
+    id: "4",
+    name: "Earth",
+    location: "Milkyway Galaxy",
+    distance: "54.6m Km",
+    gravity: "9.807 m/s ",
+    description: "Lorem ipsum...",
+    image: "assets/img/earth.png",
+  ),
+  Planet(
+    id: "5",
+    name: "Mercury",
+    location: "Milkyway Galaxy",
+    distance: "54.6m Km",
+    gravity: "3.7 m/s ",
+    description: "Lorem ipsum...",
+    image: "assets/img/mercury.png",
+  ),
+];
