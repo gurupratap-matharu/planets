dbbd532bb00760cc5b71d803aae83f8f2416a487
gurupratap-matharu <gurupratap.matharu@gmail.com>
2019-11-30 10:42:29 -0300

    UI optimizations based on user feedback

--- lib/ui/home/home_page_body.dart
+++ lib/ui/home/home_page_body.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:planets/ui/home/planet_row.dart';
+import 'package:planets/models/planet_data.dart';
 
 class HomePageBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return PlanetRow();
+    return PlanetRow(
+      planet: planets[0],
+    );
   }
 }
--- lib/ui/home/planet_row.dart
+++ lib/ui/home/planet_row.dart
@@ -1,50 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:planets/models/planet.dart';
 
 class PlanetRow extends StatelessWidget {
+  PlanetRow({this.planet});
+
+  final Planet planet;
+
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: 120.0,
       margin: EdgeInsets.symmetric(
         vertical: 16.0,
         horizontal: 24.0,
       ),
       child: Stack(
         children: <Widget>[
-          planetCard,
-          planetThumbnail,
+          Container(
+            height: 124.0,
+            margin: EdgeInsets.only(
+              left: 46.0,
+            ),
+            decoration: BoxDecoration(
+              color: Color(0xFF333366),
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.circular(8.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black12,
+                  blurRadius: 10.0,
+                  offset: Offset(0.0, 10.0),
+                )
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(
+              vertical: 16.0,
+            ),
+            alignment: FractionalOffset.centerLeft,
+            child: Image(
+              image: AssetImage(planet.image),
+              height: 92.0,
+              width: 92.0,
+            ),
+          ),
         ],
       ),
     );
   }
 }
-
-final planetThumbnail = Container(
-  margin: EdgeInsets.symmetric(
-    vertical: 16.0,
-  ),
-  alignment: FractionalOffset.centerLeft,
-  child: Image(
-    image: AssetImage("assets/images/mars.png"),
-    height: 92.0,
-    width: 92.0,
-  ),
-);
-
-final planetCard = Container(
-  height: 124.0,
-  margin: EdgeInsets.only(
-    left: 46.0,
-  ),
-  decoration: BoxDecoration(
-    color: Color(0xFF333366),
-    shape: BoxShape.rectangle,
-    borderRadius: BorderRadius.circular(8.0),
-    boxShadow: [
-      BoxShadow(
-        color: Colors.black12,
-        blurRadius: 10.0,
-        offset: Offset(0.0, 10.0),
-      )
-    ],
-  ),
-);
